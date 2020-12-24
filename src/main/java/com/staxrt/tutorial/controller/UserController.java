317099fee3a160b898b8f49004f896caf9f3469a
leeming0619 <leeming0619@hotmail.com>
2020-12-23 19:31:24 -0600

    f

---
+++ src/main/java/com/staxrt/tutorial/controller/UserController.java
@@ -0,0 +1,166 @@
+/*
+ *
+ *  Copyright (c) 2018-2020 Xing Jin, This source is a part of
+ *   Staxrt - sample application source code.
+ *   http://staxrt.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+package com.staxrt.tutorial.controller;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.staxrt.tutorial.exception.ResourceNotFoundException;
+import com.staxrt.tutorial.model.Login;
+import com.staxrt.tutorial.model.User;
+import com.staxrt.tutorial.repository.UserRepository;
+
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.apache.tomcat.util.json.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityManager;
+import javax.validation.Valid;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * The type User controller.
+ *
+ * @author Xing Jin
+ */
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/v1")
+public class UserController {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  /**
+   * Create user user.
+   *
+   * @param user the user
+   * @return the user
+   * @throws Exception
+   */
+  @PostMapping("/login")
+  public ResponseEntity<String>  getUsersByEmail(@RequestBody Login data) throws Exception {
+    List<User> result = userRepository.findAll();
+    //Logger LOGGER=LoggerFactory.getLogger(UserController.class);
+    for (int i = 0; i < result.size(); i++) {
+      //LOGGER.info(data.getEmail());
+      if (data.getEmail().equals(result.get(i).getEmail())) {
+        return new ResponseEntity<>("{\"success\" : true, \"token\" : \"e23m30ewrtr3e3df3des3dfdfd3ery76895\" }", HttpStatus.OK);
+      }     
+    }
+    return new ResponseEntity<>("{\"status\" : \"false\"}", HttpStatus.OK);
+  }
+
+  /**
+   * Get all users list.
+   *
+   * @return the list
+   */
+  @GetMapping("/users")
+  public List<User> getAllUsers() {
+    return userRepository.findAll();
+  }
+
+  /**
+   * Gets users by id.
+   *
+   * @param userId the user id
+   * @return the users by id
+   * @throws ResourceNotFoundException the resource not found exception
+   */
+  @GetMapping("/users/{id}")
+  public ResponseEntity<User> getUsersById(@PathVariable(value = "id") Long userId)
+      throws ResourceNotFoundException {
+    User user =
+        userRepository
+            .findById(userId)
+            .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
+    return ResponseEntity.ok().body(user);
+  }
+
+  /**
+   * Create user user.
+   *
+   * @param user the user
+   * @return the user
+   */
+  @PostMapping("/users")
+  public User createUser(@Valid @RequestBody User user) {
+    return userRepository.save(user);
+  }
+
+  /**
+   * Update user response entity.
+   *
+   * @param userId the user id
+   * @param userDetails the user details
+   * @return the response entity
+   * @throws ResourceNotFoundException the resource not found exception
+   */
+  @PutMapping("/users/{id}")
+  public ResponseEntity<User> updateUser(
+      @PathVariable(value = "id") Long userId, @Valid @RequestBody User userDetails)
+      throws ResourceNotFoundException {
+
+    User user =
+        userRepository
+            .findById(userId)
+            .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
+
+    user.setEmail(userDetails.getEmail());
+    user.setLastName(userDetails.getLastName());
+    user.setFirstName(userDetails.getFirstName());
+    user.setUpdatedAt(new Date());
+    final User updatedUser = userRepository.save(user);
+    return ResponseEntity.ok(updatedUser);
+  }
+
+  /**
+   * Delete user map.
+   *
+   * @param userId the user id
+   * @return the map
+   * @throws Exception the exception
+   */
+  @DeleteMapping("/user/{id}")
+  public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId) throws Exception {
+    User user =
+        userRepository
+            .findById(userId)
+            .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
+
+    userRepository.delete(user);
+    Map<String, Boolean> response = new HashMap<>();
+    response.put("deleted", Boolean.TRUE);
+    return response;
+  }
+}
